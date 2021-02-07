161f5a1440a37126a918e63dd7f1ef3fc48c202f
sinanjurequiem <gabrieltd31t@outlook.com>
2021-02-07 15:28:00 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,6 @@
+const { Client } = require('discord.js');
+const bot = new Client();
+
+bot.on('ready', () => console.log(`Logged in as ${bot.user.username}`));
+
+bot.login ('ODAzMzg0MDE0NjE4NjI0MDgx.YA8_qw.fgthvaBuUGi2_U5LHCylgisWoQM');
