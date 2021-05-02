81760b9caa21701915edfefc0329a57a951e0c89
Michael <s.michaelru@gmail.com>
2021-05-02 05:03:47 +0000

    updated help
    Signed-off-by: Michael <s.michaelru@gmail.com>

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -10,19 +10,18 @@ module.exports = {
 			var helpembed = new Discord.MessageEmbed()
 				.setTitle("Economy Help")
 				.addFields(
-					{ name: "`$start`", value: "open an account at the meme bank.", inline: true },
-					{ name: "`$daily`", value: "get your daily dose of internet. jk, get your daily paycheck.", inline: true },
-					{ name: "`$hourly`", value: "get your hourly dose of caffeine and work. YOU NEED MONEY, NO TIME FOR SLEEP.", inline: true },
-					{ name: "`$stats`", value: "get your stats, including bank balance", inline: true },
-					{ name: "`$buyminer`", value: "buy or upgrade your crypto miner. crypto miners allow you to generate income while afk.", inline: true },
-					{ name: "`$shop`", value: "see the stonks on sale. you can't buy directly from it though.", inline: true},
-					{ name: "`$buystonks`", value: "buy stonks. to use this command, after typing buystonks, type the number of stonks you want to buy, then the first 4 letters of the stonks you want to buy. complex, i know. but this is an economy. what did you expect?", inline: true},
-					{ name: "`$sellstonks`", value: "sell stonks. for usage, check the entry for buystonks.", inline: true},
-					{ name: "`$help`", value: "what do you think you're looking at?", inline: true },
-					{ name: "`$ping`", value: "this is won't give you your ping, or ping another user, nor will it reply with 'pong'. it is merely a test command.", inline: true },
-					{ name: "`$patreon`", value: "donate on patreon. everything donated goes toward keeping the bot running.", inline: true },
-					{ name: "`$vote`", value: "vote for the bot on top.gg and get rewards, up to $750000! so vote.", inline: true},
-
+					{ name: "`$start`", value: "open an account at the meme bank."},
+					{ name: "`$daily`", value: "get your daily dose of internet. jk, get your daily paycheck."},
+					{ name: "`$hourly`", value: "get your hourly dose of caffeine and work. YOU NEED MONEY, NO TIME FOR SLEEP."},
+					{ name: "`$vote`", value: "vote for the bot on top.gg and get rewards!"},
+					{ name: "`$stats`", value: "get your stats, including bank balance"},
+					{ name: "`$leaderboard`", value: "show the 10 people around the world with the highest bank balance"},
+					{ name: "`$buyminer`", value: "buy or upgrade your crypto miner. crypto miners allow you to generate income while afk."},
+					{ name: "`$shop`", value: "see the stonks on sale."},
+					{ name: "`$buystonks`", value: "$buystonks <qty> <first 4 letters of stock>"},
+					{ name: "`$sellstonks`", value: "sell stonks. for usage, check the entry for buystonks."},
+					{ name: "`$help`", value: "what do you think you're looking at?"},
+					{ name: "`$patreon`", value: "donate on patreon. everything donated goes toward keeping the bot running."}
 				)
 				.setDescription(`yeah, i know. an economy is complicated. here's a list of my commands and their usage. btw my prefix is \`${prefix}\``)
 
