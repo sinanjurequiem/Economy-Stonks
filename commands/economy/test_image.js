e7880446a148606d1914a83da82f0b18e81a9872
Michael <s.michaelru@gmail.com>
2021-05-29 05:44:19 +0000

    basic canvas wallpaper
    Signed-off-by: Michael <s.michaelru@gmail.com>

--- commands/economy/test_image.js
+++ commands/economy/test_image.js
@@ -3,71 +3,27 @@ const Discord = require('discord.js');
 const config = require('../../configuration/config.json');
 var helper = require('../../helpers.js');
 
+import { CanvasTable, CTConfig } from "canvas-table";
+
+
 module.exports = {
 	name: "image",
-	description: "test node-html-to-image",
-	execute(msg, dbClient, args) {
-    if (args.length == 0){
-      msg.reply("no args")
-      return;
-    }
-    name = args[0]
-    const _htmlTemplate = `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <style>
-        body {
-          font-family: "Poppins", Arial, Helvetica, sans-serif;
-          background: rgb(22, 22, 22);
-          color: #fff;
-          max-width: 300px;
-        }
+	description: "test node-html-to-image"
+}
+module.exports.execute = async function (msg, dbClient, args) {
+    const canvas = Canvas.createCanvas(700, 250);
+	  const context = canvas.getContext('2d');
 
-        .app {
-          max-width: 300px;
-          padding: 20px;
-          display: flex;
-          flex-direction: row;
-          border-top: 3px solid rgb(16, 180, 209);
-          background: rgb(31, 31, 31);
-          align-items: center;
-        }
+    // Since the image takes time to load, you should await it
+    const background = await Canvas.loadImage('./wallpaper.jpg');
+    // This uses the canvas dimensions to stretch the image onto the entire canvas
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+    // Set the color of the stroke
+    context.strokeStyle = '#74037b';
+    // Draw a rectangle with the dimensions of the entire canvas
+    context.strokeRect(0, 0, canvas.width, canvas.height);
+    // Use the helpful Attachment class structure to process the file for you
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
-        img {
-          width: 50px;
-          height: 50px;
-          margin-right: 20px;
-          border-radius: 50%;
-          border: 1px solid #fff;
-          padding: 5px;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="app">
-        <img src="https://avatars.dicebear.com/4.5/api/avataaars/${name}.svg" />
-
-        <h4>Welcome ${name}</h4>
-      </div>
-    </body>
-  </html>
-  `
-
-    const images = nodeHtmlToImage({
-      html: _htmlTemplate,
-      quality: 100,
-      type: 'jpeg',
-      puppeteerArgs: {
-        args: ['--no-sandbox'],
-      },
-      encoding: 'buffer',
-    }).then(function(result,err){
-      msg.channel.send(new MessageAttachment(images, `${name}.jpeg`));
-    }).catch(err => {console.log(err)});
-  // for more configuration options refer to the library
-
-  }
+    msg.reply(attachment);
 }
