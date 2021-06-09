479fbc12b121efea19771fc3ce5849e813f89654
Michael <s.michaelru@gmail.com>
2021-06-09 04:09:26 +0000

    add stonks rating, change command name
    Signed-off-by: Michael <s.michaelru@gmail.com>

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 var helper = require('../../helpers.js');
 
 module.exports = {
-	name: "shop",
+	name: "stonks",
 	description: "see the stonks on sale",
   status: "enabled",
 	execute(msg, dbClient, args){
@@ -15,9 +15,21 @@ module.exports = {
 
       for (var i = 0; i < result.length; i++){
         var oldest = result[i].history[result[i].history.length-1];
-        var change = (result[i].value - oldest)
-        var sign = change > 0 ? ':arrow_up_small:' : ':small_red_triangle_down:'
-        shopEmbed.addField(`${result[i].name} [${result[i].ticker.toUpperCase()}] $${helper.formatNumber(result[i].value.toFixed(2))}`, `${sign} ${helper.formatNumber(change.toFixed(2))} (${helper.formatNumber((change*100/oldest).toFixed(2))}%)`)
+        var change = (result[i].value - oldest);
+        var sign = change > 0 ? ':arrow_up_small:' : ':small_red_triangle_down:';
+        var rating = `neutral`
+        switch (true){
+          case result[i].demand > 5:
+            rating = `BUY`;
+            break;
+          case result[i].demand < -5:
+            rating = `SELL`;
+            break;
+          default:
+            rating = `neutral`;
+            break;
+        }
+        shopEmbed.addField(`${result[i].name} [${result[i].ticker.toUpperCase()}] $${helper.formatNumber(result[i].value.toFixed(2))}`, `${sign} ${helper.formatNumber(change.toFixed(2))} (${helper.formatNumber((change*100/oldest).toFixed(2))}%)\tRating: ${rating}`)
       }
       msg.reply(shopEmbed);
     });
