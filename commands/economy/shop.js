afd88d107f80c53591c318ade4e292c7a7fdbd7a
Michael <s.michaelru@gmail.com>
2021-06-17 04:01:30 +0000

    shows real stocks
    Signed-off-by: Michael <s.michaelru@gmail.com>

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -1,38 +1,36 @@
 const Discord = require('discord.js');
 var helper = require('../../helpers.js');
+const yahooFinance = require('yahoo-finance');
 
 module.exports = {
   name: "stonks",
   description: "see the stonks on sale",
-  status: "enabled",
-  execute(msg, dbClient, args) {
+  status: "enabled"
+}
 
-    var dbo = dbClient.db("economy");
-    var promise = dbo.collection("bank").find({}).toArray().then(function(result, err) {
-      var shopEmbed = new Discord.MessageEmbed()
-        .setTitle("Shop")
-        .setDescription("this is the stonks market. go buy some stonks. The gain shown is over the last 5 hours.")
-
-      for (var i = 0; i < result.length; i++) {
-        var oldest = result[i].history[result[i].history.length - 1];
-        var change = (result[i].value - oldest);
-        var sign = change > 0 ? ':arrow_up_small:' : ':small_red_triangle_down:';
-        var rating = `neutral`
-        switch (true) {
-          case result[i].demand > 5:
-            rating = `BUY`;
-            break;
-          case result[i].demand < -5:
-            rating = `SELL`;
-            break;
-          default:
-            rating = `neutral`;
-            break;
-        }
-        shopEmbed.addField(`${result[i].name} [${result[i].ticker.toUpperCase()}] $${helper.formatNumber(result[i].value.toFixed(2))}`, `${sign} ${helper.formatNumber(change.toFixed(2))} (${helper.formatNumber((change * 100 / oldest).toFixed(2))}%)\tRating: ${rating}`)
-      }
-      msg.reply(shopEmbed);
-    }).catch(err => { console.log(err); return err });
-    return promise;
+module.exports.execute = async function(msg, dbClient, args) {
+  var shopEmbed = new Discord.MessageEmbed()
+    .setTitle("Stonks")
+    .setDescription("this is the stonks market.")
+  var symbols = [];
+  if (args.length == 0) {
+    symbols = ['AAPL', 'asdf', 'AMC', 'TSLA'];
+  } else {
+    symbols = args;
   }
+
+  for (var i = 0; i < symbols.length; i++) {
+
+    const result = await yahooFinance.quote(symbols[i], ['price']);
+
+    if (result.price.shortName == null) {
+
+      continue;
+    }
+
+    var change = (result.price.regularMarketPrice - result.price.regularMarketOpen);
+
+    shopEmbed.addField(`${result.price.shortName} [${symbols[i]}] $${result.price.regularMarketPrice}`, `${helper.formatNumber(change.toFixed(2))} (${helper.formatNumber((change * 100 / result.price.regularMarketOpen).toFixed(2))}%)`)
+  }
+  msg.reply(shopEmbed);
 }
